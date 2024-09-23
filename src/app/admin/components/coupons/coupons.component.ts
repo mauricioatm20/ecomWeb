efa87a5d0be909e0deb34fcc655af9d8cc475e0f
mauricio <mauriciochasi@gmail.com>
2024-09-23 23:49:29 +0200

    table-coupons

--- src/app/admin/components/coupons/coupons.component.ts
+++ src/app/admin/components/coupons/coupons.component.ts
@@ -1,12 +1,49 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {AdminService} from "../../service/admin.service";
+import {MatCard, MatCardContent} from "@angular/material/card";
+import {
+  MatCell,
+  MatCellDef,
+  MatColumnDef,
+  MatHeaderCell,
+  MatHeaderCellDef,
+  MatHeaderRow, MatRow,
+  MatTableModule
+} from "@angular/material/table";
+import {CommonModule} from "@angular/common";
+import {ReactiveFormsModule} from "@angular/forms";
 
 @Component({
   selector: 'app-coupons',
   standalone: true,
-  imports: [],
+  imports: [
+    MatCard,
+    MatCardContent,
+    MatTableModule,
+    MatColumnDef,
+    MatHeaderCell,
+    MatHeaderCellDef,
+    MatCell,
+    MatCellDef,
+    MatHeaderRow,
+    MatRow,
+    CommonModule,
+    ReactiveFormsModule,
+  ],
   templateUrl: './coupons.component.html',
-  styleUrl: './coupons.component.css'
+  styleUrl: './coupons.component.scss'
 })
-export class CouponsComponent {
+export class CouponsComponent implements OnInit{
 
+  coupons: any;
+  constructor(
+    private adminService: AdminService,
+  ){}
+
+  ngOnInit(): void {
+    this.getCoupons();
+  }
+  getCoupons(): void{
+    this.adminService.getCoupons().subscribe(res => this.coupons = res);
+  }
 }
