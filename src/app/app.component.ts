b7694c22f453e28210ac5760bb6659bed23669e2
mauricio <mauriciochasi@gmail.com>
2024-10-26 01:40:03 +0200

    navbar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,19 +1,20 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, HostListener, OnInit} from '@angular/core';
 import {Router, RouterLink, RouterLinkActive, RouterOutlet} from '@angular/router';
 import {MatToolbar} from "@angular/material/toolbar";
-import {MatButton} from "@angular/material/button";
+import {MatButton, MatIconButton} from "@angular/material/button";
 import {LoginComponent} from "./login/login.component";
 import {SignupComponent} from "./signup/signup.component";
 import {UserStorageService} from "./service/storage/user-storage.service";
 import {CommonModule} from "@angular/common";
-import {MatMenu} from "@angular/material/menu";
+import {MatMenu, MatMenuModule} from "@angular/material/menu";
+import {MatIcon} from "@angular/material/icon";
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CommonModule, MatToolbar, MatButton, RouterLink, RouterLinkActive, LoginComponent, SignupComponent, MatMenu],
+  imports: [RouterOutlet, CommonModule, MatToolbar, MatButton, RouterLink, RouterLinkActive, LoginComponent, SignupComponent, MatMenuModule, MatIcon, MatIconButton],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
@@ -35,5 +36,54 @@ export class AppComponent implements OnInit{
   logOut(): void {
     UserStorageService.signOut();
     this.router.navigateByUrl('login')
+  }
+
+  // Elementos del menú
+  adminMenuItems = [
+    { label: 'Dashboard', link: '/dashboard' },
+    { label: 'Analytics', link: '/admin/analytics' },
+    { label: 'Category', link: '/admin/category' },
+    { label: 'Product', link: '/admin/product' },
+    { label: 'Orders', link: '/admin/orders' },
+    { label: 'Post Coupon', link: '/admin/post-coupon' },
+    { label: 'Coupons', link: '/admin/coupons' },
+    { label: 'Logout', action: () => this.logOut() }, // Agrega este elemento
+  ];
+
+  // Define el umbral para pantallas pequeñas
+  private readonly SMALL_SCREEN_THRESHOLD = 1100;
+
+  // Método para comprobar si la pantalla es pequeña
+  isSmallScreen(): boolean {
+    return window.innerWidth < this.SMALL_SCREEN_THRESHOLD;
+  }
+
+  // Listener para eventos de cambio de tamaño de la ventana
+  @HostListener('window:resize', ['$event'])
+  onResize(event) {
+    // Esto ayudará a actualizar la UI de acuerdo al tamaño
+  }
+
+  // Elementos del menú para clientes
+  customerMenuItems = [
+    { label: 'Dashboard', link: '/dashboard-c' },
+    { label: 'Cart', link: '/customer/cart' },
+    { label: 'Orders', link: '/customer/my_orders' },
+    { label: 'Wishlist', link: '/customer/wishlist' },
+    { label: 'Logout', action: () => this.logOut() }, // Agrega este elemento
+  ];
+
+  // Define el umbral para pantallas pequeñas
+  private readonly SMALL_SCREEN_THRESHOLDC = 1000;
+
+  // Método para comprobar si la pantalla es pequeña
+  isSmallScreenC(): boolean {
+    return window.innerWidth < this.SMALL_SCREEN_THRESHOLDC;
+  }
+
+  // Listener para eventos de cambio de tamaño de la ventana
+  @HostListener('window:resize', ['$event'])
+  onResizeC(event) {
+    // Esto ayudará a actualizar la UI de acuerdo al tamaño
   }
 }
